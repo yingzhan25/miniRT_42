71990bd25e129a4c0f0d04f94014b1cd96c13e03
Javokhir Kubaev <jkubaev7@gmail.com>
2025-12-02 23:23:04 +0100

    vector functions

---
+++ src/math/vector_ops.c
@@ -0,0 +1,43 @@
+#include "../../includes/vector.h"
+
+t_vec3	vec_add(t_vec3 a, t_vec3 b)
+{
+	t_vec3	tmp;
+
+	tmp.x = a.x + b.x;
+	tmp.y = a.y + b.y;
+	tmp.z = a.z + b.z;
+	return (tmp);
+}
+
+t_vec3	vec_sub(t_vec3 a, t_vec3 b)
+{
+	t_vec3	tmp;
+
+	tmp.x = a.x - b.x;
+	tmp.y = a.y - b.y;
+	tmp.z = a.z - b.z;
+	return (tmp);
+}
+
+t_vec3	vec_scale(t_vec3 v, double scalar)
+{
+	t_vec3	tmp;
+
+	tmp.x = v.x * scalar;
+	tmp.y = v.y * scalar;
+	tmp.z = v.z * scalar;
+	return (tmp);
+}
+
+t_vec3	vec_divide(t_vec3 v, double scalar)
+{
+	t_vec3	tmp;
+
+	tmp.x = v.x / scalar;
+	tmp.y = v.y / scalar;
+	tmp.z = v.z / scalar;
+	return (tmp);
+}
+
+
