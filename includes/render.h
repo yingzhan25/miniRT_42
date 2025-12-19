199908aa8cd5440e229627cdb38f6b51a0d4bd45
yingzhan25 <3146589226@qq.com>
2025-12-19 16:34:51 +0100

    complete mlx functions

--- includes/render.h
+++ includes/render.h
@@ -4,11 +4,13 @@
 # include "scene.h"
 # include <math.h>
 # include "/usr/local/include/mlx.h"
+# include <X11/X.h>
 
 // Number of pixels, can be changed to 1920*1080
 # define WIN_WIDTH 800
 # define WIN_HEIGHT 600
 # define M_PI 3.14159265358979323846
+# define KEY_ESC 65307
 
 // Ray definition
 typedef struct s_ray {
@@ -32,5 +34,10 @@ void	setup_camera(t_camera *cam);
 void	setup_viewport(t_camera *cam);
 void	render_scene(t_mlx_data *data);
 t_ray	generate_ray(t_camera *cam, int i, int j);
+
+// Window management
+void	mlx_data_init(t_mlx_data *data, t_scene *scene);
+void	mlx_put_color_to_image(t_mlx_data *data, t_color color, int x, int y);
+void	handle_events(t_mlx_data *data);
 
 #endif
