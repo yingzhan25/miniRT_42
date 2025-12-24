8dc0af5c90aba0089af35ab07f6ac21e849b85d1
yingzhan25 <3146589226@qq.com>
2025-12-24 13:06:48 +0100

    change plane normal

--- src/lighting/diffuse.c
+++ src/lighting/diffuse.c
@@ -3,12 +3,15 @@
 /**
  * Calculate color of object with both ambient and diffuse;
  * dot_product: cosine between 2 vectors;
- * cos(0) = 1; cos(pi/2) = 0; cos(pi) = -1
+ * cos(0) = 1; cos(pi/2) = 0; cos(pi) = -1;
+ * flip plane normal when hit_to_ray and plane normal in opposite direction
  */
 void	calculate_diffuse(t_color *color, t_vec3 hit_to_light, t_hit *hit, t_scene *scene)
 {
 	double	light_dot_normal;
 
+	if (hit->object->type == OBJ_PLANE && dot_product(hit_to_light, hit->normal) < 0)
+		hit->normal = vec_scale(hit->normal, -1.0);
 	light_dot_normal = dot_product(hit_to_light, hit->normal);
 	if (light_dot_normal < 0)
 		light_dot_normal = 0;
