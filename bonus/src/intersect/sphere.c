d0d685855628402c588e17777b225fb794cb5c77
yingzhan25 <3146589226@qq.com>
2026-01-05 16:58:36 +0100

    fix 42logo

--- bonus/src/intersect/sphere.c
+++ bonus/src/intersect/sphere.c
@@ -3,10 +3,10 @@
 /*
 ** Ray-Sphere Intersection
 ** This function calculates the intersection of a ray with a sphere.
-** It returns a t_intersection struct containing 
+** It returns a t_intersection struct containing
 ** the intersection points t1 and t2.
 ** If there is no intersection, the valid field is set to 0.
-** If there is an intersection, valid is set to 1 and t1, t2 
+** If there is an intersection, valid is set to 1 and t1, t2
 ** are the intersection distances.
 */
 t_intersection	ray_sphere_intersection(t_ray ray, t_sphere sphere)
@@ -19,7 +19,7 @@ t_intersection	ray_sphere_intersection(t_ray ray, t_sphere sphere)
 
 	a = dot_product(ray.direction, ray.direction);
 	b = 2 * dot_product(ray.direction, vec_sub(ray.origin, sphere.center));
-	c = dot_product(vec_sub(ray.origin, sphere.center), 
+	c = dot_product(vec_sub(ray.origin, sphere.center),
 			vec_sub(ray.origin, sphere.center)) - sphere.radius * sphere.radius;
 	discriminant = b * b - 4 * a * c;
 	if (discriminant < 0)
